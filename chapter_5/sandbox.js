205656afcbc5a7f6d7a84d500901f222531c70d7
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-20 07:24:50 +0800

    done

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -5,17 +5,19 @@ let user = {
   age: 30,
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
-  blogs: ['why mac & cheese rules', '10 things to make with marmite']
-};
+  blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+}
 
-console.log(user);
-console.log(user.age);
+console.log(user)
+console.log(user.age)
 
-user.age = 35;
-console.log(user.age);
+user.age = 35
+console.log(user.age)
 
-console.log(user['name']);
-user['name'] = 'chun-li';
-console.log(user['name']);
+const key = 'location'
 
-console.log(typeof user);
+console.log(user[key])
+user['name'] = 'chun-li'
+console.log(user['name'])
+
+console.log(typeof user)
