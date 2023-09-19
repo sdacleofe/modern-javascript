c9ca3f64f0e0e18346430796b461da8fd122ed87
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-20 07:11:03 +0800

    done

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,12 +1,23 @@
 // callbacks & forEach
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+// const myFunc = (callbackFunc) => {
+//   let value = 50
+//   callbackFunc(value)
+// }
+
+// myFunc(function (value) {
+// do something
+//   console.log(value)
+// })
+
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li']
 
 const logPerson = (person, index) => {
-  console.log(`${index} - hello ${person}`);
+  console.log(`${index} - hello ${person}`)
 }
 
-// people.forEach(person => {
-//   console.log(`hello ${person}`);
-// });
+people.forEach(logPerson)
 
-people.forEach(logPerson);
+// people.forEach((person, index) => {
+//   console.log(index, person)
+// })
