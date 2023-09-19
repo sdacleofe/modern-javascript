c8da9bbcd5be03b6e5d323853aa05956c1ac48c0
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-20 07:37:55 +0800

    done

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -6,21 +6,22 @@ let user = {
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
   blogs: ['why mac & cheese rules', '10 things to make with marmite'],
-  login(){
-    console.log('the user logged in');
+  login() {
+    console.log('the user logged in')
   },
-  logout(){
-    console.log('the user logged out');
+  logout() {
+    console.log('the user logged out')
   },
-  logBlogs(){
-    // access the blogs here
-    // console.log(this);
-    console.log('this user has written these blogs:');
-    this.blogs.forEach(blog => {
-      console.log(blog);
+  logBlogs: function () {
+    // access the blogs using 'this' keyword
+    // console.log(this.blogs)
+    console.log('this user has written the following blogs:')
+    this.blogs.forEach((blog) => {
+      console.log(blog)
     })
-  }
-};
+    console.log(this)
+  },
+}
 
-// console.log(this);
-user.logBlogs();
+user.logBlogs()
+console.log(this)
