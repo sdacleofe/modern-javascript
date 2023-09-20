dc0da0c249b9f39e05ca207d5c0d114d1227a095
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-20 10:28:38 +0800

    done

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,15 +1,21 @@
-// const content = document.querySelector('p');
+// const content = document.querySelector('p')
 
-// console.log(content.classList);
-// content.classList.add('error');
-// content.classList.remove('success');
+// console.log(content.classList)
+// content.classList.add('error')
+// content.classList.remove('error')
+// content.classList.add('success')
 
-const paras = document.querySelectorAll('p');
+const paras = document.querySelectorAll('p')
 
-paras.forEach(p => {
-  if(p.textContent.includes('error')){
-    p.classList.add('error');
-  } else if(p.textContent.includes('success')) {
-    p.classList.add('success');
+paras.forEach((para) => {
+  if (para.textContent.match('error')) {
+    para.classList.add('error')
+  } else if (para.textContent.match('success')) {
+    para.classList.add('success')
   }
-});
+})
+
+const title = document.querySelector('.title')
+
+title.classList.toggle('active')
+title.classList.toggle('active')
