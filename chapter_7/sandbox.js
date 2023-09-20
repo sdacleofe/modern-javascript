2d621029f8b95a37714195e7d005579aaf0d77cd
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-20 16:55:44 +0800

    done

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,23 +1,22 @@
-const form = document.querySelector('.signup-form');
+const form = document.querySelector('.signup-form')
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  console.log(form.username.value);
-});
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log(form.username.value)
+})
 
 // testing RegEx
+const username = 'shanice123'
+const pattern = /^[a-z]{6,}/
 
-const username = 'shaunyp';
-const pattern = /^[a-z]{6,}$/;
+// let result = pattern.test(username)
+// console.log(result)
 
-// let result = pattern.test(username);
-
-// if(result){
-//   console.log('regex test passed :)');
+// if (result) {
+//   console.log('regex test passed')
 // } else {
-//   console.log('regex test failed :(');
+//   console.log('regex test failed')
 // }
 
-let result = username.search(pattern);
-
-console.log(result);
+let result = username.search(pattern)
+console.log(result)
