4425599b2724f80a1345da5b356274a05ac2b323
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-20 15:55:27 +0800

    done

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,17 +1,16 @@
-const copy = document.querySelector('.copy-me');
+const copy = document.querySelector('.copy-me')
 
 copy.addEventListener('copy', () => {
-  console.log('OI! my content is copyrighted!!');
-});
+  console.log('OI!my content is copyright')
+})
 
-const box = document.querySelector('.box');
+const box = document.querySelector('.box')
+box.addEventListener('mousemove', (e) => {
+  console.log('mouse is moving within the box!')
+  console.log(e.offsetX, e.offsetY)
+  box.textContent = `x pos - ${e.offsetX} y pos - ${e.offsetY}`
+})
 
-box.addEventListener('mousemove', e => {
-  // console.log(e);
-  // console.log(e.offsetX, e.offsetY);
-  box.textContent = `x pos - ${e.offsetX}  y pos - ${e.offsetY}`;
-});
-
-document.addEventListener('wheel', e => {
-  console.log(e.pageX, e.pageY);
-});
+document.addEventListener('wheel', (e) => {
+  console.log(e.pageX, e.pageY)
+})
