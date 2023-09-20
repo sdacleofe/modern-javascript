20ce688c5ad2b01021b478fac3daa4233e6657d6
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-20 09:34:41 +0800

    done

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,11 +1,11 @@
-const link = document.querySelector('a');
+const link = document.querySelector('a')
 
-console.log(link.getAttribute('href'));
-link.setAttribute('href', 'https://www.thenetninja.co.uk');
-link.textContent = 'The Net Ninja webiste';
+console.log(link.getAttribute('href'))
+link.setAttribute('href', 'https://www.google.com.ph')
+link.innerText = 'Google Website'
 
-const mssg = document.querySelector('p');
+const mssg = document.querySelector('p')
 
-console.log(mssg.getAttribute('class'));
-mssg.setAttribute('class', 'success');
-mssg.setAttribute('style', 'color: green');
+console.log(mssg.getAttribute('class'))
+mssg.setAttribute('class', 'success')
+mssg.setAttribute('style', 'color: green')
