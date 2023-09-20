80c0f616a15668eded4c301f56df4fa28e43f685
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-20 16:31:09 +0800

    done

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,9 +1,9 @@
-const form = document.querySelector('.signup-form');
-// const username = document.querySelector('#username');
+const form = document.querySelector('.signup-form')
+// const username = document.querySelector('#username')
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  // console.log('form submitted');
-  // console.log(username.value);
-  console.log(form.username.value);
-});
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  // console.log(e)
+  // console.log(username.value)
+  console.log(form.username.value)
+})
