2d3601dad89fcf78106305b79eb7862bd3e59e04
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-22 07:29:51 +0800

    done

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,4 +1,7 @@
-const scores = [10, 5, 0, 40, 60, 10, 20, 70];
+const scores = [10, 5, 0, 40, 90, 10, 30, 70]
 
-const firstHighScore = scores.find(score => score > 50);
-console.log(firstHighScore);
+const firstHighScore = scores.find((score) => {
+  return score > 50
+})
+
+console.log(firstHighScore)
