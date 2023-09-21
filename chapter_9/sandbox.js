06f39999997aa210d96dd205c0d05419bd17ece9
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-22 07:26:48 +0800

    done

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,4 +1,13 @@
-// const scores = [10, 20, 60, 40, 70, 90, 30];
+const scores = [10, 20, 60, 40, 70, 90, 30]
+
+const result = scores.reduce((acc, curr) => {
+  if (curr > 50) {
+    acc++
+  }
+  return acc
+}, 0)
+
+console.log(result)
 
 // const result = scores.reduce((acc, curr) => {
 //   if(curr > 50){
@@ -9,18 +18,27 @@
 
 // console.log(result);
 
-const scores = [
-  {player: 'mario', score: 50},
-  {player: 'yoshi', score: 30},
-  {player: 'mario', score: 70},
-  {player: 'crystal', score: 60}
-];
+const scores2 = [
+  { player: 'mario', score: 50 },
+  { player: 'yoshi', score: 30 },
+  { player: 'mario', score: 70 },
+  { player: 'crystal', score: 60 },
+]
 
-const marioTotal = scores.reduce((acc, curr) => {
-  if(curr.player === 'mario'){
-    acc += curr.score;
+const marioTotal = scores2.reduce((acc, curr) => {
+  if (curr.player === 'mario') {
+    acc += curr.score
   }
-  return acc;
-}, 0);
+  return acc
+}, 0)
 
-console.log(marioTotal);
+console.log(marioTotal)
+
+// const marioTotal = scores.reduce((acc, curr) => {
+//   if(curr.player === 'mario'){
+//     acc += curr.score;
+//   }
+//   return acc;
+// }, 0);
+
+// console.log(marioTotal);
