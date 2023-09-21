a22173e54de1b83122337ca258a84e89cb1bb940
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-22 07:48:43 +0800

    done

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,10 +1,22 @@
 const products = [
-  {name: 'gold star', price: 30},
-  {name: 'green shell', price: 10},
-  {name: 'red shell', price: 40},
-  {name: 'banana skin', price: 5},
-  {name: 'mushroom', price: 50}
-];
+  { name: 'gold star', price: 30 },
+  { name: 'green shell', price: 10 },
+  { name: 'red shell', price: 40 },
+  { name: 'banana skin', price: 5 },
+  { name: 'mushroom', price: 50 },
+]
+
+// const filtered = products.filter((product) => product.price > 20)
+// console.log(filtered)
+
+// const promos = filtered.map((product) => {
+//   return `the ${product.name} is ${product.price / 2} pounds`
+// })
+// console.log(promos)
+
+const promos = products
+  .filter((product) => product.price > 20)
+  .map((product) => `the ${product.name} is ${product.price / 2} pounds`)
 
 // const filtered = products.filter(product => product.price > 20);
 
@@ -12,8 +24,8 @@ const products = [
 //   return `the ${product.name} is ${product.price / 2} pounds`;
 // });
 
-const promos = products
-  .filter(product => product.price > 20)
-  .map(product => `the ${product.name} is ${product.price / 2} pounds`);
+// const promos = products
+//   .filter(product => product.price > 20)
+//   .map(product => `the ${product.name} is ${product.price / 2} pounds`);
 
-console.log(promos);
+console.log(promos)
