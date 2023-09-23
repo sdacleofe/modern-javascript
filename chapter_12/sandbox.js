167147e999ba16553d8ac5a6faf09e3e6a7b9c9d
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-23 09:33:38 +0800

    done

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,12 +1,24 @@
-const request = new XMLHttpRequest();
+const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', () => {
-  // console.log(request, request.readyState);
-  if(request.readyState === 4){
-    // console.log(request);
-    console.log(request.responseText);
-  } 
-});
+  // console.log(request, request.readyState)
+  if (request.readyState === 4) {
+    console.log(request.responseText)
+  }
+})
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+request.send()
+
+// const request = new XMLHttpRequest();
+
+// request.addEventListener('readystatechange', () => {
+//   // console.log(request, request.readyState);
+//   if(request.readyState === 4){
+//     // console.log(request);
+//     console.log(request.responseText);
+//   }
+// });
+
+// request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+// request.send();
