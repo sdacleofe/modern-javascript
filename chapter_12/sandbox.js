47f71f4066cee4f4c1473c8f7b90c12ca886f9c1
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-23 09:42:19 +0800

    done

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,13 +1,13 @@
-const request = new XMLHttpRequest();
+const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', () => {
   // console.log(request);
-  if(request.readyState === 4 && request.status === 200){
-    console.log(request.responseText);
-  } else if (request.readyState === 4){
-    console.log('could not fetch the data');
+  if (request.readyState === 4 && request.status === 200) {
+    console.log(request, request.responseText)
+  } else if (request.readyState === 4) {
+    console.log('could not fetch data')
   }
-});
+})
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/')
+request.send()
