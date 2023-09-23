a82dbe59a2bc50f29df61a057d5756578b5926e3
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-23 11:43:49 +0800

    done

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,33 +1,31 @@
 const getTodos = (callback) => {
-
-  const request = new XMLHttpRequest();
+  const request = new XMLHttpRequest()
 
   request.addEventListener('readystatechange', () => {
-
-    if(request.readyState === 4 && request.status === 200){
-      callback(undefined, request.responseText);
-    } else if (request.readyState === 4){
-      callback('could not fetch the data', undefined);
+    // console.log(request);
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText)
+    } else if (request.readyState === 4) {
+      callback('could not fetch data', undefined)
     }
+  })
 
-  });
-  
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-  request.send();
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+  request.send()
+}
 
-};
-
-console.log(1);
-console.log(2);
+console.log(1)
+console.log(2)
 
 getTodos((err, data) => {
-  console.log('callback function fired');
-  if(err){
-    console.log(err);
+  // console.log('callback is fired')
+  console.log(err, data)
+  if (err) {
+    console.log(err)
   } else {
-    console.log(data);
+    console.log(data)
   }
-});
+})
 
-console.log(3);
-console.log(4);
+console.log(3)
+console.log(4)
