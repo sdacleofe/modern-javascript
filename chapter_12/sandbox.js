c8ac4eafb890151a507a99ed599dff0058cce660
Shanice Dawn Cleofe <sdacleofewd@gmail.com>
2023-09-23 12:20:40 +0800

    done

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,33 +1,32 @@
 const getTodos = (resource) => {
-
   return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-
+    const request = new XMLHttpRequest()
     request.addEventListener('readystatechange', () => {
-  
-      if(request.readyState === 4 && request.status === 200){
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4){
-        reject('could not fetch the data');
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText)
+        resolve(data)
+      } else if (request.readyState === 4) {
+        reject('error getting resource')
       }
-  
-    });
-    
-    request.open('GET', resource);
-    request.send();
-  });
+    })
 
-};
+    request.open('GET', resource)
+    request.send()
+  })
+}
 
-getTodos('json/luigi.json').then(data => {
-  console.log('promise 1 resolved:', data);
-  return getTodos('json/mario.json');
-}).then(data => {
-  console.log('promise 2 resolved:', data);
-  return getTodos('json/shaun.json');
-}).then(data => {
-  console.log('promise 3 resolved:', data);
-}).catch(err => {
-  console.log('promise rejected:', err);
-});
+getTodos('json/luigi.json')
+  .then((data) => {
+    console.log('promise 1 resolved:', data)
+    return getTodos('json/mario.json')
+  })
+  .then((data) => {
+    console.log('promise 2 resolved:', data)
+    return getTodos('json/shaun.json')
+  })
+  .then((data) => {
+    console.log('promise 3 resolved:', data)
+  })
+  .catch((err) => {
+    console.log('promise rejected:', err)
+  })
