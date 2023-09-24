ab4f807c24cda48a632b9b76b4cbd55877823b2c
Shanice Dawn <sdacleofewd@gmail.com>
2023-09-25 07:15:59 +0800

    done

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,20 +1,33 @@
 // async & await
 
 const getTodos = async () => {
+  const response = await fetch('json/luigi.json')
+  const data = await response.json()
+  return data
+}
 
-  let response = await fetch('json/luigi.json');
-  let data = await response.json();
-  return data;
-
-};
-
-console.log(1);
-console.log(2);
-
+console.log(1) 
+// this is non blocking
 getTodos()
-  .then(data => console.log('resolved:', data));
+.then(data => console.log('resolved:', data))
+console.log(2)
 
-console.log(3);
-console.log(4);
 
-// console.log(getTodos());
+// const getTodos = async () => {
+
+//   let response = await fetch('json/luigi.json');
+//   let data = await response.json();
+//   return data;
+
+// };
+
+// console.log(1);
+// console.log(2);
+
+// getTodos()
+//   .then(data => console.log('resolved:', data));
+
+// console.log(3);
+// console.log(4);
+
+// // console.log(getTodos());
