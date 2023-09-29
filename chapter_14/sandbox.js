8e3fc2e183b1bccdbc04450ee0624400d2987c30
Shanice Dawn <sdacleofewd@gmail.com>
2023-09-29 13:09:53 +0800

    done

--- chapter_14/sandbox.js
+++ chapter_14/sandbox.js
@@ -1,18 +1,19 @@
 // store data in local storage
-localStorage.setItem('name', 'mario');
-localStorage.setItem('age', '50');
+localStorage.setItem('name', 'mario')
+localStorage.setItem('age', 50)
 
 // get data from local storage
-let name = localStorage.getItem('name');
-let age = localStorage.getItem('age');
+let name = localStorage.getItem('name')
+let age = localStorage.getItem('age')
 
-console.log(name, age);
+console.log(name)
+console.log(age)
 
 // updating data
-localStorage.setItem('name', 'luigi');
-localStorage.age = '40';
+localStorage.setItem('name', 'luigi')
+localStorage.age = '40'
 
-name = localStorage.getItem('name');
-age = localStorage.getItem('age');
-
-console.log(name, age);
+name = localStorage.getItem('name')
+age = localStorage.getItem('age')
+console.log(name)
+console.log(age)
