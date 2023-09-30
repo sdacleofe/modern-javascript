7b2a664608f5ea42e56cf3caaa9548b76b838267
Shanice Dawn <sdacleofewd@gmail.com>
2023-09-30 14:25:23 +0800

    done

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,33 +1,42 @@
 class User {
-  constructor(username, email){
-    this.username = username;
-    this.email = email;
-    this.score = 0;
+  constructor(username, email, score) {
+    // setup properties
+    this.username = username
+    this.email = email
+    this.score = 0
   }
-  login(){
-    console.log(`${this.username} just logged in`);
-    return this;
+
+  login() {
+    console.log(`${this.username} just logged in`)
+    return this
   }
-  logout(){
-    console.log(`${this.username} just logged out`);
-    return this;
+
+  logout() {
+    console.log(`${this.username} just logged out`)
+    return this
   }
-  incScore(){
-    this.score += 1;
-    console.log(`${this.username} has a score of ${this.score}`);
-    return this;
+
+  incScore() {
+    this.score += 1
+    console.log(`${this.username} has a score of ${this.score}`)
+    return this
   }
+
 }
 
-const userOne = new User('luigi', 'luigi@thenetninja.co.uk');
-const userTwo = new User('mario', 'mario@thenetninja.co.uk');
+const userOne = new User("mario", 'mario@gmail.com')
+const userTwo = new User("luigi", 'luigi@gmail.com')
 
-// userOne.login();
-// userOne.logout();
-// userTwo.login();
-// userTwo.logout();
+console.log(userTwo, userTwo)
+userOne.login()
+userTwo.login().incScore().incScore()
+userOne.logout()
+userTwo.logout()
 
-userTwo.login()
-  .incScore()
-  .incScore()
-  .logout();
+userOne.incScore()
+userOne.incScore()
+
+// the 'new' keyword
+// 1 - it creates a new empty object {}
+// 2 - it binds the value of 'this' to the new empty object
+// 3 - it calls the constructor function to 'build' the object
