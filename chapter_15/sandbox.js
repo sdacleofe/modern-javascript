441957e0bf1d2f305d610864b86c0533bc56d9a1
Shanice Dawn <sdacleofewd@gmail.com>
2023-09-30 15:49:50 +0800

    done

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,23 +1,24 @@
 // constructor functions
 // class User {
-//   constructor(username){
+//   constructor(username, email){
 //     this.username = username;
+//     this.email = email
 //   }
 // }
 
-function User(username, email){
-  this.username = username;
-  this.email = email;
-  this.login = function(){
-    console.log(`${this.username} has logged in`);
-  };
+function User(username, email) {
+  this.username = username
+  this.email = email
+  this.login = function() {
+    console.log(`${this.username} has logged in`)
+  }
 }
 
 const userOne = new User('ryu', 'ryu@thenetninja.co.uk');
 const userTwo = new User('chun-li', 'chun.li@thenetninja.co.uk');
 
-console.log(userOne, userTwo);
-userOne.login();
+console.log(userOne, userTwo)
+userOne.login()
 
 // the 'new' keyword
 // 1 - it creates a new empty object {}
